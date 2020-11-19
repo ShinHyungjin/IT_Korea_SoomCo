2f21173343095c7814771f049c00125073aa06fe
ISPserver <tjdals080@naver.com>
2020-11-19 21:41:40 +0900

    page add

---
+++ SoomCo/src/com/soomco/main/MainFrame.java
@@ -0,0 +1,12 @@
+package com.soomco.main;
+
+public class MainFrame {
+
+	public MainFrame() {
+	
+	}
+	public static void main(String[] args) {
+		System.out.println("hello world!!!!!!!!!!!!");
+	}
+	
+}
